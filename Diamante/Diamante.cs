e3b01070163e0c830c695acb1df0b597176cead7
Victor Muniz <victor.hm2000@aluno.ifsc.edu.br>
2021-06-24 17:35:29 -0300

    Finalizado

---
+++ Diamante/Diamante.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Diamante
+{
+    public class Diamante
+    {
+        readonly String[] letras = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+        private readonly char letra;
+
+        public Diamante(char letra)
+        {
+            this.letra = letra;
+        }
+
+        public String MontarDiamante()
+        {
+            int espacosExterior = ObterPosicaoDaLetra(letra)-1 ;
+            String diamanteStr = "";
+            int espacosInteriorSuperior = 1;
+            int espacosInteriorInferior = 0;
+        
+            for (int i = 0; i < ObterPosicaoDaLetra(letra) -1; i++)
+            {
+                String espacosExteriorStr = new string(' ', espacosExterior);
+                String espacosInteriorSuperiorStr = new string(' ', espacosInteriorSuperior);
+                if (letras[i] == "A")
+                {
+                    diamanteStr += espacosExteriorStr + letras[i] + "\n";
+                }
+                else
+                {
+                    diamanteStr += espacosExteriorStr + letras[i] + espacosInteriorSuperiorStr + letras[i] + "\n";
+                    espacosInteriorSuperior += 2;
+                }
+                espacosExterior -= 1;
+                espacosInteriorInferior = espacosInteriorSuperior;
+            }
+            for (int i = ObterPosicaoDaLetra(letra) - 1; i >= 0; i--)
+            {
+                String espacosExteriorStr = new string(' ', espacosExterior);
+                String espacosInteriorInferiorStr = new string(' ', (int)espacosInteriorInferior);
+                if (letras[i] == "A")
+                {
+                    diamanteStr += espacosExteriorStr + letras[i] + "\n";
+                }
+                else
+                {
+                    diamanteStr += espacosExteriorStr + letras[i] + espacosInteriorInferiorStr + letras[i] + "\n";
+                    if (letras[i] != "B") espacosInteriorInferior -= 2;
+                }
+                espacosExterior += 1;
+            }
+
+            return diamanteStr;
+        }
+
+        private int ObterPosicaoDaLetra(char letra)
+        {
+            return (int)letra % 32;
+        }
+    }
+}
---
+++ Diamante/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Diamante
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite uma letra para gerar o diamante: ");
+            char letra = (char)Console.Read();
+            Console.WriteLine();
+
+            Diamante d1 = new Diamante(letra);
+
+            Console.WriteLine(d1.MontarDiamante());
+            Console.ReadKey();
+        }
+    }
+}
